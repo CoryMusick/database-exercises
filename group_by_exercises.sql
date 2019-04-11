d1c79ad862a132807ffe1e240b8a4359e2003224
Cory Musick <cory.musick37@gmail.com>
2019-04-11 14:33:29 -0500

    some chnages to group by and aliases is complete

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -5,10 +5,11 @@ select distinct title from titles;
 
 select distinct last_name from employees where last_name like 'E%e';
 
-select distinct first_name, last_name from employees where last_name like 'E%e';
+select first_name, last_name from employees where last_name like 'E%e'
+GROUP BY last_name, first_name;
 
-select last_name, count(last_name) from employees where last_name regexp 'q' and last_name not regexp 'qu'
-group by last_name;
+select last_name, count(last_name) from employees where last_name like '%q%' and last_name not like '%qu%'
+group by last_name order by count(last_name) desc;
 
-select count(*), gender from employees where first_name IN ('Irena', 'Vidya','Maya')
+select count(*) as 'has name', gender from employees where first_name IN ('Irena', 'Vidya','Maya')
 group by gender;
